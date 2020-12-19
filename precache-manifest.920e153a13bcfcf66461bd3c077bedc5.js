611356b80287e095a02da1c536654ad8d21cbb34
Aijaz Ahmed Patel <developers@virasatinfotech.com>
2020-12-19 18:57:26 +0530

    Updates

---
+++ precache-manifest.920e153a13bcfcf66461bd3c077bedc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef23d6c6dfdda0605bec3dc4362e4d77",
+    "url": "/Aijazpatel/slotweb.git/index.html"
+  },
+  {
+    "revision": "50d3cd2f9aa05725b419",
+    "url": "/Aijazpatel/slotweb.git/static/css/main.a620fbd6.chunk.css"
+  },
+  {
+    "revision": "45e12010c71b7804aa12",
+    "url": "/Aijazpatel/slotweb.git/static/js/2.99d84dbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aijazpatel/slotweb.git/static/js/2.99d84dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d3cd2f9aa05725b419",
+    "url": "/Aijazpatel/slotweb.git/static/js/main.a5ff4546.chunk.js"
+  },
+  {
+    "revision": "b7aabbce97c488d290e0",
+    "url": "/Aijazpatel/slotweb.git/static/js/runtime-main.844d2f7d.js"
+  }
+]);
